9e17102c3719addf81086153f0d75149e6b3cf26
yangyang-lgtm <yang.yang@smartlogictech.com>
2025-08-29 10:22:26 +0000

    init custom dialect

---
+++ include/CustomDialect/CustomDialect.h
@@ -0,0 +1,12 @@
+//
+// Created by ubuntu on 2025/8/29.
+//
+
+#ifndef TRITON_TO_CUSTOM_CUSTOMDIALECT_H
+#define TRITON_TO_CUSTOM_CUSTOMDIALECT_H
+
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/IR/MLIRContext.h"
+#include "CustomDialect/CustomDialect.h.inc"
+
+#endif //TRITON_TO_CUSTOM_CUSTOMDIALECT_H
---
+++ lib/CustomDialect/CustomDialect.cpp
@@ -0,0 +1,10 @@
+#include "CustomDialect/CustomDialect.h"
+
+#include "llvm/Support/raw_ostream.h"
+#include "CustomDialect/CustomDialect.cpp.inc"
+
+namespace mlir::custom{
+
+void CustomDialect::initialize() {}
+
+}
